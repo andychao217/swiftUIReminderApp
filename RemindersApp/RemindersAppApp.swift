a22b7a8da28fb8119390eac319ae6c3f3a8e7e3e
Andy Chao <zhaoqing@spon.com.cn>
2024-11-15 10:56:18 +0800

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Andy Chao on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
